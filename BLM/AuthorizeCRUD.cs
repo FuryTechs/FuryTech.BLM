73bb148d9c72fe93e8f7aba6793598dd1779aac6
gallayl <gallay.lajos@gmail.com>
2017-01-19 14:47:20 +0100

    Add: AuthorizeCRUD to public API

---
+++ BLM/AuthorizeCRUD.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using BLM.Interfaces;
+using BLM.Interfaces.Authorize;
+
+namespace BLM
+{
+    public abstract class AuthorizeCRUD<T> : AuthorizeCollection<T>, IAuthorizeCreate<T>, IAuthorizeModify<T>, IAuthorizeRemove<T>
+    {
+        public abstract Task<AuthorizationResult> CanCreateAsync(T entity, IContextInfo ctx);
+        public abstract Task<AuthorizationResult> CanModifyAsync(T originalEntity, T modifiedEntity, IContextInfo ctx);
+
+        public abstract Task<AuthorizationResult> CanRemoveAsync(T entity, IContextInfo ctx);
+
+    }
+}
