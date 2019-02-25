414276804b73b602bbe44b3de98abdf65cd4b225
carathorys <gallayb@gmail.com>
2019-02-25 12:10:37 +0100

    Added some docs

--- src/NetStandard/AuthorizeCRUD.cs
+++ src/NetStandard/AuthorizeCRUD.cs
@@ -4,11 +4,35 @@ using FuryTechs.BLM.NetStandard.Interfaces.Authorize;
 
 namespace FuryTechs.BLM.NetStandard
 {
+    /// <summary>
+    /// Abstract class to authorize user for CRUD operations
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public abstract class AuthorizeCRUD<T> : AuthorizeCollection<T>, IAuthorizeCreate<T>, IAuthorizeModify<T>, IAuthorizeRemove<T> where T : class
     {
+        /// <summary>
+        /// Authorizes user if it can create the entity
+        /// </summary>
+        /// <param name="entity">Entity to create</param>
+        /// <param name="ctx">Context information</param>
+        /// <returns>Authorization result (deny/accept)</returns>
         public abstract Task<AuthorizationResult> CanCreateAsync(T entity, IContextInfo ctx);
+
+        /// <summary>
+        /// Authorizes user if it can update the entity
+        /// </summary>
+        /// <param name="originalEntity">Original entity</param>
+        /// <param name="modifiedEntity">Modified entity</param>
+        /// <param name="ctx">Context information</param>
+        /// <returns>Authorization result (deny/accept)</returns>
         public abstract Task<AuthorizationResult> CanModifyAsync(T originalEntity, T modifiedEntity, IContextInfo ctx);
 
+        /// <summary>
+        /// Authorizes user if it can delete the entity
+        /// </summary>
+        /// <param name="entity">Entity to delete</param>
+        /// <param name="ctx">Context information</param>
+        /// <returns>Authorization result (deny/accept)</returns>
         public abstract Task<AuthorizationResult> CanRemoveAsync(T entity, IContextInfo ctx);
 
     }
