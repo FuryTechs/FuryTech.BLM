0225bb3f59297df356211b978c41f85295f6439b
carathorys <gallayb@gmail.com>
2023-01-20 08:53:06 +0100

    Update to latest .NET 6, and 7

---
+++ src/EntityFrameworkCore/IRepository.cs
@@ -0,0 +1,12 @@
+using FuryTechs.BLM.NetStandard.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace FuryTechs.BLM.EntityFrameworkCore
+{
+  public interface IRepository<T, TDbContext> : IRepository<T>
+    where T: class
+    where TDbContext: DbContext
+  {
+    
+  }
+}
--- tests/NetStandard.Tests/MockNestedEntity.cs
+++ tests/NetStandard.Tests/MockNestedEntity.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace FuryTechs.BLM.NetStandard.Tests
 {
     public class MockNestedEntity
     {
+        [Key]
         public int Id { get; set; }
         public virtual List<MockEntity> MockEntities { get; set; }
     }
